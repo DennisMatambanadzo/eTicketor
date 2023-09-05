50aa61d4c39dd8a181e045022419edef74e6ff11
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2023-09-05 10:49:01 +0200

    configure websecurity

--- src/main/java/online/epochsolutions/eticketor/api/auth/config/WebSecurityConfig.java
+++ src/main/java/online/epochsolutions/eticketor/api/auth/config/WebSecurityConfig.java
@@ -2,10 +2,19 @@ package online.epochsolutions.eticketor.api.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+import static online.epochsolutions.eticketor.models.user.Permission.*;
+import static online.epochsolutions.eticketor.models.user.Permission.ADMIN_READ;
+import static online.epochsolutions.eticketor.models.user.Role.*;
+import static online.epochsolutions.eticketor.models.user.Role.ADMIN;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.GET;
 
 @Configuration
 public class WebSecurityConfig {
@@ -20,8 +29,41 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
-        http.authorizeHttpRequests(authorize ->authorize.requestMatchers("/auth/register","/auth/login","/auth/me", "/auth/verify").permitAll()
-                .anyRequest().authenticated());
+        http.authorizeHttpRequests(authorize ->authorize.requestMatchers("/auth/**").permitAll());
+
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("eTicketor/event/**").hasAnyRole(HOST.name(), ADMIN.name())
+                .requestMatchers(GET,"eTicketor/event/**").hasAnyAuthority(ADMIN_READ.name(), HOST_READ.name())
+                .requestMatchers(POST,"eTicketor/event/**").hasAuthority( HOST_CREATE.name())
+                .requestMatchers(PUT,"eTicketor/event/**").hasAuthority( HOST_UPDATE.name())
+                .requestMatchers(DELETE,"eTicketor/event/**").hasAnyAuthority(ADMIN_DELETE.name(), HOST_DELETE.name()));
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("eTicketor/ticket/**").hasRole(USER.name())
+                .requestMatchers(POST,"eTicketor/ticket/**").hasAuthority(ADMIN_CREATE.name()));
+
+
+        http.authorizeHttpRequests(authorize ->authorize
+                .requestMatchers("admin/**").hasRole(ADMIN.name())
+                .requestMatchers(GET, "admin/**").hasAuthority(ADMIN_READ.name()).anyRequest().authenticated());
+
+//        http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET,"/auth/roleHierarchy").hasRole("USER"));
         return http.build();
     }
+
+    @Bean
+    public RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_ADMIN > ROLE_STAFF \n ROLE_STAFF > ROLE_USER";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
+
+//    @Bean
+//    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
+//        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+//        expressionHandler.setRoleHierarchy(roleHierarchy());
+//        return expressionHandler;
+//    }
 }
